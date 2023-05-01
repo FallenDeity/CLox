f500d553f5b5684a2cf8f67ef824dca8ee00ab44
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-05-01 16:34:09 +0530

    feat: update workflows

---
+++ src/common.h
@@ -0,0 +1,12 @@
+//
+// Created by Triyan Mukherjee on 01-05-2023.
+//
+
+#ifndef LOX_COMMON_H
+#define LOX_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif//LOX_COMMON_H
